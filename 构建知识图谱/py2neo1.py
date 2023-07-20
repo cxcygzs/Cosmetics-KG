e78dab1c498025081e58a9676bafad3285600856
hua <2291113485@qq.com>
2023-07-20 18:11:14 +0800

    将一些没有name属性的节点的sub_name属性的值作为节点的name属性的值

--- 构建知识图谱/py2neo1.py
+++ 构建知识图谱/py2neo1.py
@@ -8,14 +8,17 @@ graph = Graph("bolt://localhost:7687", auth=("neo4j", "12345678"))
 def import_nodes_from_csv():
     with open('nodes.csv', 'r', encoding='utf-8') as file:
         lines = file.readlines()
-        property = ['brand','code','engName','goods_id','id','image','max','min','name','price','size','sub_name','typeName']
+        property = lines[0].strip().split(',')[2:]
+        # property = ['brand','code','engName','goods_id','id','image','max','min','name','price','size','sub_name','typeName']
         for line in lines[1:]:
             row = line.strip().split(',')
+            print(row)
             _id = int(row[0])
             labels = row[1].split(':')[1]
             print(labels)
             properties = {key: value for key, value in zip(property, row[2:]) if value and key}
-
+            if not row[10]:
+                properties['name'] = row[13]
             node = Node(labels, _id=_id, **properties)
             graph.create(node)
 
